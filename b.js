b799510e742b0221a0f9b39d9cd880576b9afc9f
shiva-1612 <shivakumarssk2304@gmail.com>
2023-03-20 22:37:24 +0530

    first commit

---
+++ b.js
@@ -0,0 +1,12 @@
+
+function mypayment(){
+var x=prompt("Enter Email address");
+var y=prompt("Enter Amount");
+var s= new Audio("a.mp3");
+s.play();
+
+    alert("Your transaction is successful !"); 
+
+
+}
+ 
